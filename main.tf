53b0949c8ba654200262a6b1cb852eb1943ee0b9
Ameya Rane <amey.rane86@gmail.com>
2026-01-04 14:35:42 +0530

    update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,12 @@ provider "aws" {
 
 resource "aws_instance" "jenkins_demo" {
   ami           = "ami-0ebfed9ccce07b642"
-  instance_type = var.instance_type
+  instance_type = "t2.micro"
+  key_name      =  "fqts-demo-key"
+  vpc_security_group_ids = ["sg-0fc76108e42f52851"]
   tags = {
     Name = "jenkins-${terraform.workspace}"
     Environment = "${terraform.workspace}"
   }
 }
 
-variable "instance_type" {
-  default = "t2.micro"
-}
